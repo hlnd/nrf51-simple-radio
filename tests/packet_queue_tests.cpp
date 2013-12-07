e8c012f70249fcf1e983b78f3e22ec779433b2bc
Ole Morten <om@haaland.biz>
2013-12-07 23:12:00 +0100

    Add test to check that queue is FIFO.

--- tests/packet_queue_tests.cpp
+++ tests/packet_queue_tests.cpp
@@ -54,3 +54,22 @@ TEST(packet_queue, test_queue_full_queue_is_full)
     LONGS_EQUAL(true, packet_queue_is_full(&queue));
 }
     
+TEST(packet_queue, test_queue_is_fifo)
+{
+    radio_packet_t in_packets[PACKET_QUEUE_SIZE];
+
+    for (int i = 0; i < PACKET_QUEUE_SIZE; i++)
+    {
+        in_packets[i].data[0] = rand()*INT_MAX;
+        packet_queue_add(&queue, &in_packets[i]);
+    }
+
+    for (int i = 0; i < PACKET_QUEUE_SIZE; i++)
+    {
+        radio_packet_t * p_packet = 0;
+        packet_queue_get(&queue, &p_packet);
+
+        CHECK(0 != p_packet);
+        LONGS_EQUAL(0, memcmp(p_packet, &in_packets[i], sizeof(in_packets[i])));
+    }
+}

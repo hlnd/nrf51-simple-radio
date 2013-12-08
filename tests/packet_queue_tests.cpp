3b0e79d11b03fb9e5fa1e8129ae967e09f1e4013
Ole Morten <om@haaland.biz>
2013-12-08 01:48:07 +0100

    Add tests for getting out elements.

--- tests/packet_queue_tests.cpp
+++ tests/packet_queue_tests.cpp
@@ -93,3 +93,63 @@ TEST(packet_queue, test_queue_get_fails_on_empty_queue)
 
 }
 
+TEST(packet_queue, test_queue_get_returns_correct_pointer)
+{
+    radio_packet_t in_packet;
+    packet_queue_add(&queue, &in_packet);
+
+    radio_packet_t * out_packet = 0;
+    packet_queue_get(&queue, &out_packet);
+
+    POINTERS_EQUAL(out_packet, &queue.packets[0]);
+}
+
+TEST(packet_queue, test_queue_add_get_packet)
+{
+    radio_packet_t in_packet;
+    in_packet.data[0] = rand()*INT_MAX;
+    packet_queue_add(&queue, &in_packet);
+
+    radio_packet_t * out_packet;
+    packet_queue_get(&queue, &out_packet);
+    
+    LONGS_EQUAL(0, memcmp(&in_packet, out_packet, sizeof(in_packet)));
+}
+    
+TEST(packet_queue, test_queue_is_fifo)
+{
+    radio_packet_t in_packets[PACKET_QUEUE_ELEMENTS];
+
+    for (int i = 0; i < PACKET_QUEUE_ELEMENTS; i++)
+    {
+        in_packets[i].data[0] = rand()*INT_MAX;
+        LONGS_EQUAL(SUCCESS, packet_queue_add(&queue, &in_packets[i]));
+    }
+
+    radio_packet_t * out_packets[PACKET_QUEUE_ELEMENTS] = {0};
+    for (int i = 0; i < PACKET_QUEUE_ELEMENTS; i++)
+    {
+        LONGS_EQUAL(SUCCESS, packet_queue_get(&queue, &out_packets[i]));
+    }
+
+    CHECK(0 != out_packets[0]);
+    LONGS_EQUAL(0, memcmp(out_packets[0], &in_packets[0], sizeof(in_packets[0])));
+    CHECK(0 != out_packets[1]);
+    LONGS_EQUAL(0, memcmp(out_packets[1], &in_packets[1], sizeof(in_packets[1])));
+    CHECK(0 != out_packets[2]);
+    LONGS_EQUAL(0, memcmp(out_packets[2], &in_packets[2], sizeof(in_packets[2])));
+    CHECK(0 != out_packets[3]);
+    LONGS_EQUAL(0, memcmp(out_packets[3], &in_packets[3], sizeof(in_packets[3])));
+    CHECK(0 != out_packets[4]);
+    LONGS_EQUAL(0, memcmp(out_packets[4], &in_packets[4], sizeof(in_packets[4])));
+    CHECK(0 != out_packets[5]);
+    LONGS_EQUAL(0, memcmp(out_packets[5], &in_packets[5], sizeof(in_packets[5])));
+    CHECK(0 != out_packets[6]);
+    LONGS_EQUAL(0, memcmp(out_packets[6], &in_packets[6], sizeof(in_packets[6])));
+    CHECK(0 != out_packets[7]);
+    LONGS_EQUAL(0, memcmp(out_packets[7], &in_packets[7], sizeof(in_packets[7])));
+    CHECK(0 != out_packets[8]);
+    LONGS_EQUAL(0, memcmp(out_packets[8], &in_packets[8], sizeof(in_packets[8])));
+    CHECK(0 != out_packets[9]);
+    LONGS_EQUAL(0, memcmp(out_packets[9], &in_packets[9], sizeof(in_packets[9])));
+}

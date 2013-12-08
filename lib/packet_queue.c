99e0edf752302fe27340e16b9d8233cd04bc2029
Ole Morten <om@haaland.biz>
2013-12-08 01:34:17 +0100

    Increment head and tail properly.

--- lib/packet_queue.c
+++ lib/packet_queue.c
@@ -31,9 +31,9 @@ uint32_t packet_queue_add(packet_queue_t * queue, radio_packet_t * packet)
     if (packet_queue_is_full(queue))
         return NO_MEMORY;
 
-    memcpy(&queue->packets[0], packet, sizeof(*packet));
+    memcpy(&queue->packets[queue->tail], packet, sizeof(*packet));
 
-    queue->tail++;
+    queue->tail = (queue->tail + 1u) % PACKET_QUEUE_SIZE;
 
     return SUCCESS;
 }
@@ -45,7 +45,7 @@ uint32_t packet_queue_get(packet_queue_t * queue, radio_packet_t ** packet)
 
     *packet = &queue->packets[queue->head];
 
-    queue->head++;
+    queue->head = (queue->head + 1u) % PACKET_QUEUE_SIZE;
 
     return SUCCESS;
 }
